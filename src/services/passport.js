3b13d5489b1139909b9190265a142658c00fb75e
manaraph <manaraph225@gmail.com>
2019-11-11 10:22:18 +0100

    passport service => setup local strategy and verify user

--- src/services/passport.js
+++ src/services/passport.js
@@ -4,3 +4,18 @@ const JWTStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const { findUserById, verifyUser } = require('../actions/signIn');
 const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
+
+const localOptions = { usernameField: 'email' };
+
+const localLogin = new LocalStrategy(localOptions, (email, password, done) => {
+  return verifyUser(email).then( validUser => {
+    bcrypt.compare(password, validUser.password).then( validPassword => {
+      if (validPassword) {
+        return done(null, validUser);
+      }
+
+      done(null, false);
+    }).catch( err => done(err, false));
+  })
+})

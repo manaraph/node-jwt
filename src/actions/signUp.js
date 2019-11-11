437661604e05427e93a6ee2a0ef0ab9d90f827b6
manaraph <manaraph225@gmail.com>
2019-11-11 02:08:46 +0100

    Setup signup query

---
+++ src/actions/signUp.js
@@ -0,0 +1,13 @@
+import db from './db';
+
+const createUser = (name, email, password) => {
+  const query = `
+    INSERT INTO users
+    (name, email, password)
+    VALUES ($1, $2, $3)
+    RETURNING*
+  `;
+  return db.one(query, [name, email, password]);
+}
+
+module.exports = { createUser };

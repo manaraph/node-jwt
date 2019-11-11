9500d782873b21893ce74b180c125f3514584e5c
manaraph <manaraph225@gmail.com>
2019-11-11 03:09:38 +0100

    authentication logic

--- src/controllers/authentication.js
+++ src/controllers/authentication.js
@@ -11,3 +11,27 @@ const tokentForUser = user => {
 const siginin = (req, res, next) => {
   res.send({ token: tokentForUser(req.user)});
 }
+
+const signup = (req, res, next) => {
+  const { name, email, password } = req.body;
+  const saltRounds = 12;
+
+  if (!name || !email) {
+    res.status(422).send({ error: 'Please enter your email address and password'});
+  }
+
+  bcrypt.hash(password, saltRounds).then( hash => {
+    return createUser(name, email, hash).then( newUser => {
+      res.json({ token: tokentForUser(newUser) })
+    }).catch( err => {
+      res.json({ error: 'Error saving user to the database'})
+    })
+  }).catch( err => {
+    return next(err);
+  })
+}
+
+module.exports = { 
+  signup, 
+  siginin 
+}
